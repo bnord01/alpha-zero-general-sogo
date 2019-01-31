580923245ad6bae5b467533c4dbca613b33c5dcf
Benedikt Nordhoff <b_nord01@uni-muenster.de>
2019-01-31 15:13:52 +0100

    Some more changes

--- sogo/SogoGame.py
+++ sogo/SogoGame.py
@@ -38,33 +38,15 @@ class SogoGame(Game):
         return (new_board, -player)
 
     def getValidMoves(self, board, player):
-        pass
-        # return a fixed size binary vector
-        valids = [0]*self.getActionSize()
-        b = Board(self.n)
-        b.pieces = np.copy(board)
-        legalMoves =  b.get_legal_moves(player)
-        if len(legalMoves)==0:
-            valids[-1]=1
-            return np.array(valids)
-        for x, y in legalMoves:
-            valids[self.n*x+y]=1
-        return np.array(valids)
+        return np.array([self.valid_action(board,i) for i in 0:self.getActionSize()])        
 
     def getGameEnded(self, board, player):
-        pass
-        # return 0 if not ended, 1 if player 1 won, -1 if player 1 lost
-        # player = 1
-        b = Board(self.n)
-        b.pieces = np.copy(board)
-
-        if b.is_win(player):
-            return 1
-        if b.is_win(-player):
-            return -1
-        if b.has_legal_moves():
+        if evaluate(board[np.newaxis, :, :, :, 0]):
+            return player
+        if evaluate(board[np.newaxis, :, :, :, 1]):
+            return -player
+        if np.sum(board) == 64:
             return 0
-        # draw has a very little value 
         return 1e-4
 
     def getCanonicalForm(self, board, player):
@@ -76,21 +58,7 @@ class SogoGame(Game):
             return new_board
 
     def getSymmetries(self, board, pi):
-        pass
-        # mirror, rotational
-        assert(len(pi) == self.n**2+1)  # 1 for pass
-        pi_board = np.reshape(pi[:-1], (self.n, self.n))
-        l = []
-
-        for i in range(1, 5):
-            for j in [True, False]:
-                newB = np.rot90(board, i)
-                newPi = np.rot90(pi_board, i)
-                if j:
-                    newB = np.fliplr(newB)
-                    newPi = np.fliplr(newPi)
-                l += [(newB, list(newPi.ravel()) + [pi[-1]])]
-        return l
+        return [(board,pi)]
 
     def stringRepresentation(self, board):        
         # 8x8 numpy array (canonical board)
@@ -111,71 +79,10 @@ class SogoGame(Game):
             z += 1
         return z
 
-    def valid_action(self, state: np.ndarray, action: int) -> bool:
+    def valid_action(self, state: np.ndarray, action: int) -> int:
         'Returns whether an action is valid in the given state or leads to the opponent winning.'
-        return action == 16 or \
-            not state[self.action_x(action), self.action_y(action), 3, 0] and \
-            not state[self.action_x(action), self.action_y(action), 3, 1]
-
-    def step(self, state: np.ndarray, action: int) -> np.ndarray:
-        'Performs a valid action and returns the resulting state.'
-        new_state = np.copy(state)
-        player = state[0, 0, 0, 2]
-        new_state[:, :, :, 2] = 1 - player
-        if action < 16:
-            x = self.action_x(action)
-            y = self.action_y(action)
-            z = self.action_z(state, x, y)
-            new_state[x, y, z, player] = 1
-        return new_state
-
-    def winning_action(self, state: np.ndarray, action: int) -> bool:
-        'Returns whether a valid action leads to a winning state for the performing player.'
-        pass
-
-    def terminal_state(self, state: np.ndarray) -> bool:
-        'Returns whether a state is terminal.'
-        return np.sum(state) == 64 or not self.evaluate_state(state) == 0
-
-    def evaluate_state(self, state: np.ndarray) -> int:
-        'Returns -1 iff the current player won the game, 1 if the other player won the game or 0 otherwise.'        
-        player = state[0, 0, 0, 2]
-        if evaluate(state[np.newaxis, :, :, :, 0]):
-            return 1 if player else -1
-        if evaluate(state[np.newaxis, :, :, :, 1]):
-            return -1 if player else 1
-        return 0
-        
-        
-
-def display(board):
-    n = board.shape[0]
-
-    print("   ", end="")
-    for y in range(n):
-        print (y,"", end="")
-    print("")
-    print("  ", end="")
-    for _ in range(n):
-        print ("-", end="-")
-    print("--")
-    for y in range(n):
-        print(y, "|",end="")    # print the row #
-        for x in range(n):
-            piece = board[y][x]    # get the piece to print
-            if piece == -1: print("X ",end="")
-            elif piece == 1: print("O ",end="")
-            else:
-                if x==n:
-                    print("-",end="")
-                else:
-                    print("- ",end="")
-        print("|")
-
-    print("  ", end="")
-    for _ in range(n):
-        print ("-", end="-")
-    print("--")
-
-
-    
+        return 1 if 
+                action == 16 or \
+                not state[self.action_x(action), self.action_y(action), 3, 0] and \
+                not state[self.action_x(action), self.action_y(action), 3, 1] 
+            else 0        
