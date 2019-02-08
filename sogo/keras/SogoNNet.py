38da77cb17d5b6f60742de92ad8762539ad700db
Benedikt Nordhoff <b_nord01@uni-muenster.de>
2019-02-08 14:44:44 +0100

    NNet Inception

--- sogo/keras/SogoNNet.py
+++ sogo/keras/SogoNNet.py
@@ -1,54 +1,92 @@
 from keras.models import *
 from keras.layers import *
+from keras import layers
 from keras.optimizers import *
 
 """
 NeuralNet for the game of Sogo.
 """
+
+def conv2d_bn(x,
+              filters,
+              num_row,
+              num_col,
+              padding='same',
+              strides=(1, 1),
+              name=None):
+    """Utility function to apply conv + BN.
+    # Arguments
+        x: input tensor.
+        filters: filters in `Conv2D`.
+        num_row: height of the convolution kernel.
+        num_col: width of the convolution kernel.
+        padding: padding mode in `Conv2D`.
+        strides: strides in `Conv2D`.
+        name: name of the ops; will become `name + '_conv'`
+            for the convolution and `name + '_bn'` for the
+            batch norm layer.
+    # Returns
+        Output tensor after applying `Conv2D` and `BatchNormalization`.
+    """
+    if name is not None:
+        bn_name = name + '_bn'
+        conv_name = name + '_conv'
+    else:
+        bn_name = None
+        conv_name = None        
+    x = Conv2D(
+        filters, (num_row, num_col),
+        strides=strides,
+        padding=padding,
+        use_bias=False,
+        name=conv_name)(x)
+    x = BatchNormalization(scale=False, name=bn_name)(x)
+    x = Activation('relu', name=name)(x)
+    return x
+
+def conv3d_bn(x,
+              filters,
+              num_row,
+              num_col,
+              num_lvl,
+              padding='same',
+              strides=(1, 1, 1),
+              name=None):
+    """Utility function to apply conv + BN.
+    # Arguments
+        x: input tensor.
+        filters: filters in `Conv3D`.
+        num_row: height of the convolution kernel.
+        num_col: width of the convolution kernel.
+        num_lvl: depth of the convolution kernel.
+        padding: padding mode in `Conv3D`.
+        strides: strides in `Conv3D`.
+        name: name of the ops; will become `name + '_conv'`
+            for the convolution and `name + '_bn'` for the
+            batch norm layer.
+    # Returns
+        Output tensor after applying `Conv3D` and `BatchNormalization`.
+    """
+    if name is not None:
+        bn_name = name + '_bn'
+        conv_name = name + '_conv'
+    else:
+        bn_name = None
+        conv_name = None        
+    x = Conv3D(
+        filters, (num_row, num_col, num_lvl),
+        strides=strides,
+        padding=padding,
+        use_bias=False,
+        name=conv_name)(x)
+    x = BatchNormalization(scale=False, name=bn_name)(x)
+    x = Activation('relu', name=name)(x)
+    return x
+
 class SogoNNet():
-    def alt(self, game, args):
-        # game params
-        self.board_x, self.board_y, self.board_z = game.getBoardSize()
-        self.action_size = game.getActionSize()
-        self.args = args
+    
 
-        num_4x1 = 128
-        num_4x4 = 128
-        num_4x4x4 = 512
-
-        self.input_boards = Input(shape=(self.board_x, self.board_y, self.board_z, 2))
-        t = self.input_boards
-
-        t144 = Flatten()(Conv3D(num_4x1, (self.board_x, 1, 1), padding='valid')(t))
-        t414 = Flatten()(Conv3D(num_4x1, (1, self.board_y, 1), padding='valid')(t))
-        t441 = Flatten()(Conv3D(num_4x1, (1, 1, self.board_z), padding='valid')(t))
-
-        t114 = Flatten()(Conv3D(num_4x4, (self.board_x, self.board_y, 1), padding='valid')(t))
-        t141 = Flatten()(Conv3D(num_4x4, (self.board_x, 1, self.board_z), padding='valid')(t))
-        t411 = Flatten()(Conv3D(num_4x4, (1, self.board_y, self.board_z), padding='valid')(t))
-
-        t4   = Activation('relu')(BatchNormalization()(Dense(num_4x4x4)(Flatten()(t))))
-        t4   = Activation('relu')(BatchNormalization()(Dense(num_4x4x4)(t4)))
-
-        t = Concatenate()([t144, t414, t441, t114, t141, t411, Flatten()(t), t4])
-        t = BatchNormalization()(t)
-        t = Activation('relu')(t)
-
-        t = Dense(2048)(t)
-        t = BatchNormalization()(t)
-        t = Activation('relu')(t)
-        t = Dropout(args.dropout)(t)
-
-        t = Dense(512)(t)
-        t = BatchNormalization()(t)
-        t = Activation('relu')(t)
-        t = Dropout(args.dropout)(t)
-
-        self.pi = Dense(self.action_size, activation='softmax', name='pi')(t)   
-        self.v = Dense(1, activation='tanh', name='v')(t)
-
-        self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
-        self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args.lr))
+    
 
     def __init__(self, game, args):
         # game params
@@ -56,46 +94,84 @@ class SogoNNet():
         self.action_size = game.getActionSize()
         self.args = args
 
-        num_4x1 = 32
-        num_4x4 = 64
-        num_4x4x4 = 128
-        num_2d = 512
 
         self.input_boards = Input(shape=(self.board_x, self.board_y, self.board_z, 2))
-        t = self.input_boards
+        x = self.input_boards
 
-        t144 = Conv3D(num_4x1, (self.board_x, 1, 1), padding='same')(t)
-        t414 = Conv3D(num_4x1, (1, self.board_y, 1), padding='same')(t)
-        t441 = Conv3D(num_4x1, (1, 1, self.board_z), padding='same')(t)
+        # Upsampling to 4 x 4 x 4 x filters1
+        branch1 = conv3d_bn(x, filters1, 1, 1, 1)
 
-        t114 = Conv3D(num_4x4, (self.board_x, self.board_y, 1), padding='same')(t)
-        t141 = Conv3D(num_4x4, (self.board_x, 1, self.board_z), padding='same')(t)
-        t411 = Conv3D(num_4x4, (1, self.board_y, self.board_z), padding='same')(t)
+        branch3 = conv3d_bn(x, filters1, 1, 1, 1)
+        branch3 = conv3d_bn(branch3, filters1 * 4, 3, 3, 3)
+        branch3 = conv3d_bn(branch3, filters1, 3, 3, 3)
 
-        t4   = Conv3D(num_4x4x4, (self.board_x, self.board_y, self.board_z), padding='same')(t)
+        branch114 = conv3d_bn(x, filters1 // 2, 1, 1, 1)
+        branch114 = conv3d_bn(branch114, filters1, 1, 1, 4)
         
-        tc = Activation('relu')(BatchNormalization()(Concatenate()([t144, t414, t441, t114, t141, t411,t4])))
+        branch141 = conv3d_bn(x, filters1 // 2, 1, 1, 1)
+        branch141 = conv3d_bn(branch141, filters1, 1, 4, 1)
 
-        t = Reshape((self.board_x,self.board_y,-1))(Concatenate()([tc,t]))
+        branch411 = conv3d_bn(x, filters1 // 2, 1, 1, 1)
+        branch411 = conv3d_bn(branch411, filters1, 4, 1, 1)
 
-        t = Concatenate()([t,Activation('relu')(BatchNormalization()(Conv2D(num_2d,(4,4), padding='same')(t)))])
-        t = Concatenate()([t,Activation('relu')(BatchNormalization()(Conv2D(num_2d,(4,4), padding='same')(t)))])
-        t = Concatenate()([Reshape((self.board_x,self.board_y,-1))(self.input_boards),Activation('relu')(BatchNormalization()(Conv2D(num_2d,(4,4), padding='same')(t)))])
+        # 4 x 4 x 4 x filters1*5
+        x = concatenate(
+            [branch1, branch3, branch114, branch141, branch411],        
+            name='mixed0')
 
-        t = Flatten()(t)
+        # 4 x 4 x filters1*20
+        x = layers.reshape(x,(4,4,-1))
+
+        branch1x1 = conv2d_bn(x, 64, 1, 1)
+
+        branch4x4 = conv2d_bn(x, 48, 1, 1)
+        branch4x4 = conv2d_bn(branch5x5, 64, 4, 4)
+
+        branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+        branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+        branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+
+        branch_pool = layers.AveragePooling2D((3, 3),
+                                          strides=(1, 1),
+                                          padding='same')(x)
+        branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
+        x = layers.concatenate(
+            [branch1x1, branch4x4, branch3x3dbl, branch_pool],
+            axis=channel_axis,
+            name='mixed1')
+
         
-        t = Dense(2048)(t)
-        t = BatchNormalization()(t)
-        t = Activation('relu')(t)
-        t = Dropout(args.dropout)(t)
+        # 4 x 4
+        branch1x1 = conv2d_bn(x, 64, 1, 1)
 
-        t = Dense(512)(t)
-        t = BatchNormalization()(t)
-        t = Activation('relu')(t)
-        t = Dropout(args.dropout)(t)
+        branch4x4 = conv2d_bn(x, 48, 1, 1)
+        branch4x4 = conv2d_bn(branch5x5, 64, 1, 4)
+        branch4x4 = conv2d_bn(branch5x5, 128, 4, 1)
 
-        self.pi = Dense(self.action_size, activation='softmax', name='pi')(t)   
-        self.v = Dense(1, activation='tanh', name='v')(t)
+        branch3x3dbl = conv2d_bn(x, 64, 1, 1)
+        branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+        branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
+
+        branch_pool = layers.AveragePooling2D((3, 3),
+                                          strides=(1, 1),
+                                          padding='same')(x)
+        branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
+        x = layers.concatenate(
+            [branch1x1, branch4x4, branch3x3dbl, branch_pool],
+            axis=channel_axis,
+            name='mixed1')
+
+        x = Flatten()(x)
+        x = Dense(2048)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        
+        x = Dense(1024)(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+        self.pi = Dense(self.action_size, activation='softmax', name='pi')(x)   
+        self.v = Dense(1, activation='tanh', name='v')(x)
 
         self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
         self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args.lr))
