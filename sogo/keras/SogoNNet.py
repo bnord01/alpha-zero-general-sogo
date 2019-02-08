e7df3c43f50dbf55f082cc1c61aa478e01ce8f53
Benedikt Nordhoff <b_nord01@uni-muenster.de>
2019-02-08 15:23:11 +0100

    Format

--- sogo/keras/SogoNNet.py
+++ sogo/keras/SogoNNet.py
@@ -7,6 +7,7 @@ from keras.optimizers import *
 NeuralNet for the game of Sogo.
 """
 
+
 def conv2d_bn(x,
               filters,
               num_row,
@@ -33,7 +34,7 @@ def conv2d_bn(x,
         conv_name = name + '_conv'
     else:
         bn_name = None
-        conv_name = None        
+        conv_name = None
     x = Conv2D(
         filters, (num_row, num_col),
         strides=strides,
@@ -43,6 +44,7 @@ def conv2d_bn(x,
     x = BatchNormalization(scale=False, name=bn_name)(x)
     x = Activation('relu', name=name)(x)
     return x
+
 
 def conv3d_bn(x,
               filters,
@@ -72,7 +74,7 @@ def conv3d_bn(x,
         conv_name = name + '_conv'
     else:
         bn_name = None
-        conv_name = None        
+        conv_name = None
     x = Conv3D(
         filters, (num_row, num_col, num_lvl),
         strides=strides,
@@ -83,10 +85,8 @@ def conv3d_bn(x,
     x = Activation('relu', name=name)(x)
     return x
 
-class SogoNNet():
-    
 
-    
+class SogoNNet():
 
     def __init__(self, game, args):
         # game params
@@ -94,8 +94,8 @@ class SogoNNet():
         self.action_size = game.getActionSize()
         self.args = args
 
-
-        self.input_boards = Input(shape=(self.board_x, self.board_y, self.board_z, 2))
+        self.input_boards = Input(
+            shape=(self.board_x, self.board_y, self.board_z, 2))
         x = self.input_boards
 
         filters1 = 48
@@ -109,7 +109,7 @@ class SogoNNet():
 
         branch114 = conv3d_bn(x, filters1 // 2, 1, 1, 1)
         branch114 = conv3d_bn(branch114, filters1, 1, 1, 4)
-        
+
         branch141 = conv3d_bn(x, filters1 // 2, 1, 1, 1)
         branch141 = conv3d_bn(branch141, filters1, 1, 4, 1)
 
@@ -118,11 +118,11 @@ class SogoNNet():
 
         # 4 x 4 x 4 x filters1*5
         x = concatenate(
-            [branch1, branch3, branch114, branch141, branch411],        
+            [branch1, branch3, branch114, branch141, branch411],
             name='mixed0')
 
         # 4 x 4 x filters1*20
-        x = Reshape((4,4,-1))(x)
+        x = Reshape((4, 4, -1))(x)
 
         branch1x1 = conv2d_bn(x, 64, 1, 1)
 
@@ -134,14 +134,13 @@ class SogoNNet():
         branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
 
         branch_pool = layers.AveragePooling2D((3, 3),
-                                          strides=(1, 1),
-                                          padding='same')(x)
+                                              strides=(1, 1),
+                                              padding='same')(x)
         branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
         x = layers.concatenate(
             [branch1x1, branch4x4, branch3x3dbl, branch_pool],
             name='mixed1')
 
-        
         # 4 x 4
         branch1x1 = conv2d_bn(x, 64, 1, 1)
 
@@ -154,8 +153,8 @@ class SogoNNet():
         branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3)
 
         branch_pool = layers.AveragePooling2D((3, 3),
-                                          strides=(1, 1),
-                                          padding='same')(x)
+                                              strides=(1, 1),
+                                              padding='same')(x)
         branch_pool = conv2d_bn(branch_pool, 64, 1, 1)
         x = layers.concatenate(
             [branch1x1, branch4x4, branch3x3dbl, branch_pool],
@@ -165,13 +164,14 @@ class SogoNNet():
         x = Dense(2048, name='dense1')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
-        
+
         x = Dense(1024, name='dense2')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
 
-        self.pi = Dense(self.action_size, activation='softmax', name='pi')(x)   
+        self.pi = Dense(self.action_size, activation='softmax', name='pi')(x)
         self.v = Dense(1, activation='tanh', name='v')(x)
 
         self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
-        self.model.compile(loss=['categorical_crossentropy','mean_squared_error'], optimizer=Adam(args.lr))
+        self.model.compile(
+            loss=['categorical_crossentropy', 'mean_squared_error'], optimizer=Adam(args.lr))
