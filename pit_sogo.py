d9454f65694ead780b70325e27623fbf659ce8d0
Benedikt Nordhoff <b_nord01@uni-muenster.de>
2019-02-22 15:00:36 +0100

    new pit

--- pit_sogo.py
+++ pit_sogo.py
@@ -1,10 +1,13 @@
 import Arena
 from MCTS import MCTS
 from sogo.SogoGame import SogoGame, display
-from sogo.SogoPlayers import *
+from sogo.SogoPlayers import HumanSogoPlayer
 
 import numpy as np
-from utils import *
+from NeuralNet import NeuralNet
+from Game import Game
+
+import numpy as np
 
 """
 use this script to play any two agents against each other, or play manually with
@@ -13,9 +16,38 @@ any agent.
 
 g = SogoGame(4)
 
-# all players
-rp = RandomPlayer(g).play
-hp = HumanSogoPlayer(g).play
 
-arena = Arena.Arena(rp, hp, g, display=display)
-print(arena.playGames(2, verbose=True))
+# nnet players
+class Config(object):
+    def __init__(self):    
+      self.num_sampling_moves = 30
+      self.max_moves = 512  # for chess and shogi, 722 for Go.
+      self.numMCTSSims = 50000
+
+      # Root prior exploration noise.
+      self.root_dirichlet_alpha = 0.3  # for chess, 0.03 for Go and 0.15 for shogi.
+      self.root_exploration_fraction = 0.0
+
+      # UCB formula
+      self.pb_c_base = 19652
+      self.pb_c_init = 1.25
+
+      # Load model
+
+      self.load_model = True
+      self.load_folder_file = ('./models/','checkpoint_25.pth.tar')
+
+class NN(NeuralNet):
+  def __init__(self,game:Game):
+    self.game = game
+  def predict(self, board):
+    return np.ones(self.game.getActionSize())/self.game.getActionSize(), 0
+
+nn = NN(g)
+mcts1 = MCTS(g, nn, Config())
+n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
+
+hp = HumanSogoPlayer(g).play
+arena = Arena.Arena(n1p, hp, g, display=display)
+print(arena.playGames(20, verbose=True))
+
