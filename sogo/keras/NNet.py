1fad2aa5d425912e3fb853ff10eeadc90150dcdf
bnord <bnord01@gmail.com>
2019-03-02 10:11:57 +0100

    epochs

--- sogo/keras/NNet.py
+++ sogo/keras/NNet.py
@@ -19,7 +19,7 @@ Based on the NNet by SourKream and Surag Nair.
 args = dotdict({
     'lr': 0.001,
     'dropout': 0.3,
-    'epochs': 5,
+    'epochs': 10,
     'batch_size': 2*1024,
     'cuda': False,
     'num_channels': 512,
