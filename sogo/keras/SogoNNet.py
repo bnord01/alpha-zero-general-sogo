f37ee4d4b4c7fe61fc12c38aafe578f4852eab7c
bnord <bnord01@gmail.com>
2019-03-04 13:45:29 +0100

    Typo

--- sogo/keras/SogoNNet.py
+++ sogo/keras/SogoNNet.py
@@ -101,44 +101,47 @@ class SogoNNet():
         filters1 = 64
 
         # Upsampling to 4 x 4 x 4 x filters1
-        branch1 = conv3d_bn(x, filters1, 1, 1, 1,name='branch1')
+        branch1 = conv3d_bn(x, filters1, 1, 1, 1, name='branch1')
 
-        branch3 = conv3d_bn(x, filters1, 1, 1, 1,name='branch3_1')
-        branch3 = conv3d_bn(branch3, filters1 * 4, 3, 3, 3,name='branch3_2')
-        branch3 = conv3d_bn(branch3, filters1, 3, 3, 3,,name='branch3_3')
+        branch3 = conv3d_bn(x, filters1, 1, 1, 1, name='branch3_1')
+        branch3 = conv3d_bn(branch3, filters1 * 4, 3, 3, 3, name='branch3_2')
+        branch3 = conv3d_bn(branch3, filters1, 3, 3, 3, name='branch3_3')
 
-        branch114 = conv3d_bn(x, filters1 // 2, 1, 1, 1,name='branch114_1')
-        branch114 = conv3d_bn(branch114, filters1, 1, 1, 4,name='branch114_2')
+        branch114 = conv3d_bn(x, filters1 // 2, 1, 1, 1, name='branch114_1')
+        branch114 = conv3d_bn(branch114, filters1, 1, 1, 4, name='branch114_2')
 
-        branch141 = conv3d_bn(x, filters1 // 2, 1, 1, 1,name='branch141_1')
-        branch141 = conv3d_bn(branch141, filters1, 1, 4, 1,name='branch141_2')
+        branch141 = conv3d_bn(x, filters1 // 2, 1, 1, 1, name='branch141_1')
+        branch141 = conv3d_bn(branch141, filters1, 1, 4, 1, name='branch141_2')
 
-        branch411 = conv3d_bn(x, filters1 // 2, 1, 1, 1,name='branch411_1')
-        branch411 = conv3d_bn(branch411, filters1, 4, 1, 1,name='branch411_2')
+        branch411 = conv3d_bn(x, filters1 // 2, 1, 1, 1, name='branch411_1')
+        branch411 = conv3d_bn(branch411, filters1, 4, 1, 1, name='branch411_2')
 
         # 4 x 4 x 4 x filters1*5
         x = concatenate(
             [branch1, branch3, branch114, branch141, branch411],
             name='mixed0')
 
-
-        for i in range(1,6):
+        for i in range(1, 6):
             # 4 x 4 x filters1*20
             x = Reshape((4, 4, -1))(x)
 
-            branch1x1 = conv2d_bn(x, 64, 1, 1,name=f'mix{i}_branch1x1')
+            branch1x1 = conv2d_bn(x, 64, 1, 1, name=f'mix{i}_branch1x1')
 
-            branch4x4 = conv2d_bn(x, 48, 1, 1,name=f'mix{i}_branch4x4_1')
-            branch4x4 = conv2d_bn(branch4x4, 64, 4, 4,name=f'mix{i}_branch4x4_2')
+            branch4x4 = conv2d_bn(x, 48, 1, 1, name=f'mix{i}_branch4x4_1')
+            branch4x4 = conv2d_bn(branch4x4, 64, 4, 4,
+                                  name=f'mix{i}_branch4x4_2')
 
-            branch3x3dbl = conv2d_bn(x, 64, 1, 1,name=f'mix{i}_branch3x3dbl_1')
-            branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3,name=f'mix{i}_branch3x3dbl_2')
-            branch3x3dbl = conv2d_bn(branch3x3dbl, 96, 3, 3,name=f'mix{i}_branch3x3dbl_3')
+            branch3x3dbl = conv2d_bn(
+                x, 64, 1, 1, name=f'mix{i}_branch3x3dbl_1')
+            branch3x3dbl = conv2d_bn(
+                branch3x3dbl, 96, 3, 3, name=f'mix{i}_branch3x3dbl_2')
+            branch3x3dbl = conv2d_bn(
+                branch3x3dbl, 96, 3, 3, name=f'mix{i}_branch3x3dbl_3')
 
             branch_pool = layers.AveragePooling2D((3, 3),
-                                                strides=(1, 1),
-                                                padding='same')(x)
-            branch_pool = conv2d_bn(branch_pool, 64, 1, 1,,name=f'mix{i}_branch_pool')
+                                                  strides=(1, 1),
+                                                  padding='same')(x)
+            branch_pool = conv2d_bn(branch_pool, 64, 1, 1, , name=f'mix{i}_branch_pool')
             x = layers.concatenate(
                 [branch1x1, branch4x4, branch3x3dbl, branch_pool],
                 name='mixed{i}')
