5494ed2c45ede02fcb3f85258a05899b1f3a4b79
Benedikt Nordhoff <b_nord01@uni-muenster.de>
2019-03-05 18:18:40 +0100

    Fix graph in client

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+graph = tf.get_default_graph()
 
 from MCTS import MCTS, Play
 from sogo.SogoGame import SogoGame
@@ -22,19 +24,9 @@ config.nnet_args = NNArgs(lr=0.001,
                               batch_size=1024, 
                               epochs=20)
 
-from NeuralNet import NeuralNet
-from Game import Game
-class NN(NeuralNet):
-    def __init__(self, game: Game):
-        self.game = game
 
-    def predict(self, board):
-        return np.ones(self.game.action_size())/self.game.action_size(), 0
+nn = NNet(g,config)
 
-
-# nn = NNet(g,config)
-
-nn = NN(g)
 # nn.load_checkpoint(*(config.load_folder_file))
 mcts1 = MCTS(g, nn, config)
 
@@ -52,13 +44,14 @@ play = Play(g, g.init_board())
 
 @sio.on('move')
 def on_move(s):
-    print(s)
-    i, j = s['i'], s['j']
-    a = 4*i+j
-    play.apply(a)
-    a = ai_player(play.canonical_board())
-    play.apply(a)
-    sio.emit({'i':a//4,'j':a%4,'n':np.sum(play.canonical_board()[a//4,a%4])})
+    with graph.as_default():
+        print(s)
+        i, j = s['i'], s['j']
+        a = 4*i+j
+        play.apply(a)
+        a = ai_player(play.canonical_board())
+        play.apply(a)
+        sio.emit({'i':a//4,'j':a%4,'n':int(np.sum(play.canonical_board()[a//4,a%4]))})
    
 
 @sio.on('connect')
