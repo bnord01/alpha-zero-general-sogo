278bd194b8ad8f387c58ec820231c3d1148379e2
Benedikt Nordhoff <b_nord01@uni-muenster.de>
2019-03-14 11:54:46 +0100

    RMSprop

--- sogo/keras/large/SogoNNet.py
+++ sogo/keras/large/SogoNNet.py
@@ -168,4 +168,4 @@ class SogoNNet():
 
         self.model = Model(inputs=self.input_boards, outputs=[self.pi, self.v])
         self.model.compile(
-            loss=['categorical_crossentropy', 'mean_squared_error'], optimizer=Adam(args.lr))
+            loss=['categorical_crossentropy', 'mean_squared_error'], optimizer=RMSprop(args.lr))
